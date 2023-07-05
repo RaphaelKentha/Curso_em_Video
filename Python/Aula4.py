99ad083650f8263ae6d40dde52909977564ad380
RaphaelKentha <137506515+RaphaelKentha@users.noreply.github.com>
2023-07-05 11:53:07 -0300

    Update Aula4.py
    exercícios da aula4 modulo1 de curso de python do 'Curso em video'

--- Python/Aula4.py
+++ Python/Aula4.py
@@ -5,3 +5,34 @@
 print("Hello World!!!")
 text = input("Digite algo: ")
 print(text)
+
+print(66+4)
+print('ken', 1111)
+#testando alguns comandos basicos
+
+nome = 'ra'
+idade = 22
+peso = 70.0
+print('seu nome: ', nome, 'idade: ', idade, 'peso: ', peso)
+#melhore isso
+
+nome = input('Qual o seu nome: ')
+idade = input('Qual a sua idade: ')
+peso = input('Qual o seu peso: ')
+print('Seu nome:', nome,'\nSua idade:',idade,'\nSeu peso:',peso)
+
+#Desafio 1 da aula4,
+nome = input('Qual o seu nome: ')
+print('Ola', nome, 'prazer em conhece-lo')
+
+#desafio2 mostre a data de nascimento de alguem no formato dd/mm/aaaa
+dia = input('Em qual dia voce nasceu: ')
+mes = input('Em qual mes voce nasceu: ')
+ano = input('Em qual ano voce nasceu: ')
+print('Sua data de nascimento e: ',dia,'/',mes,'/',ano)
+
+#desafio3 leia dois nomeros e mostre a soma dos mesmos
+n1 = input('digite um numero: ')
+n2 = input('digite um numero: ')
+res = int(n1) + int(n2)
+print('A soma e: ', res)
