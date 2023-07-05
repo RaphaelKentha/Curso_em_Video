a3bb74e2204b205b957b4c88e310936b47a997e4
RaphaelKentha <137506515+RaphaelKentha@users.noreply.github.com>
2023-07-05 19:44:13 -0300

    Exercicios 1 e 2
    resolvendo tarefas simples, do curso de python

---
+++ Python_Exercicios/Exe_001.py
@@ -0,0 +1,7 @@
+print('Para ingles: 1')
+print('Para portugues: 2')
+idioma = input('Selecione a linguagem:')
+if idioma == str(1):
+    print('Hello world!!!')
+if idioma == str(2):
+    print('Ola Mundo!!!')
---
+++ Python_Exercicios/Exe_002.py
@@ -0,0 +1,5 @@
+print('#######################')
+nome = input('Me diga seu nome: ')
+print('#######################')
+print('Prazer em conhece-lo, {}!!!'.format(nome))
+print('#######################')
