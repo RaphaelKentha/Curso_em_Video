d4b3429ccfe45029be55eed7a63dc8f366200182
RaphaelKentha <137506515+RaphaelKentha@users.noreply.github.com>
2023-09-11 09:40:50 -0300

    Create Aula017.py
    Vamos aprender o que são LISTAS e como utilizar listas em Python. As listas são variáveis compostas que permitem armazenar vários valores em uma mesma estrutura, acessíveis por chaves individuais

---
+++ Python/Aula017.py
@@ -0,0 +1,119 @@
+#aula 17; modulo 3; curso em video
+# Nessa aula, vamos aprender o que são LISTAS e como utilizar listas em Python
+# As listas são variáveis compostas que permitem armazenar vários valores em 
+# uma mesma estrutura, acessíveis por chaves individuais
+
+lanche = ['Hambúrguer', 'Suco', 'Pizza', 'Pudim']
+print(lanche)
+print(lanche[1])
+print(lanche[3]) # mostra o elemento 3 da lista
+print(lanche[-2]) # mostra o penultimo elemento da lista
+print(lanche[1:3]) # mostra os elementos 1 e 2 da lista
+print(lanche[2:]) # mostra os elementos a partir do elemento 2
+lanche[3] = 'Sorvete' # altera o elemento 3 da lista
+lanche.append('Cookie') # adiciona um elemento no final da lista
+lanche.insert(1,'Pizza') # adiciona um elemento na posição 1 da lista
+lanche.insert(0, 'Cachorro Quente') # adiciona um elemento na posição 0 da lista
+del(lanche[3]) # apaga o elemento 3 da lista
+lanche.pop(3) # apaga o elemento 3 da lista
+lanche.remove('Pizza') # apaga o elemento 'Pizza' da lista
+if 'Pizza' in lanche:
+    lanche.remove('Pizza') # apaga o elemento 'Pizza' da lista
+numeros = [2, 5, 9, 1]
+numeros.sort() # coloca os elementos da lista em ordem crescente
+numeros.sort(reverse=True) # coloca os elementos da lista em ordem decrescente
+len(numeros) # mostra quantos elementos tem na lista
+numeros.insert(2, 0) # adiciona o elemento 0 na posição 2 da lista
+numeros.append(7) # adiciona o elemento 7 no final da lista
+numeros.pop() # apaga o último elemento da lista
+valores = list(range(4, 11)) # cria uma lista com os valores de 4 a 10
+valores = [8, 2, 5, 4, 9, 3, 0]
+valores.sort() # coloca os elementos da lista em ordem crescente
+for valor in valores:
+    print(f'{valor}...', end='')
+
+#desafio 78
+# Faça um programa que leia 5 valores numéricos e guarde-os em uma lista
+# No final, mostre qual foi o maior e o menor valor digitado e as suas 
+# respectivas posições na lista
+print('-='*35)
+print('{:=^81}'.format('\33[30;42mMaior e menor valor em lista\33[m'))
+print('-='*35)
+
+from time import sleep
+
+lista_cinco_valores =[]
+
+for aux in range(0, 5):
+    lista_cinco_valores.append(int(input(f'Digite um valor para a posição {aux + 1}°: ')))
+    print(f'Valor {lista_cinco_valores[(aux)]}° adicionado na posição {aux + 1}° com sucesso!')
+    sleep(0.5)
+print(f'Os seguintes valores foram digitados: {lista_cinco_valores} ', end='')
+print(f'\nO maior valor digitado foi {max(lista_cinco_valores)} nas posições ', end='')
+print(f'{lista_cinco_valores.index(max(lista_cinco_valores))}°') # mostra a posição do maior valor
+print(f'O menor valor digitado foi {min(lista_cinco_valores)} nas posições ', end='')
+print(f'{lista_cinco_valores.index(min(lista_cinco_valores))}°') # mostra a posição do menor valor
+print('\033[1;30;41m}#{|\033[m' * 20)
+
+#desafio 79
+# Crie um programa onde o usuário possa digitar vários valores numéricos e 
+# cadastre-os em uma lista
+# Caso o número já exista lá dentro, ele não será adicionado
+# No final, serão exibidos todos os valores únicos digitados, em ordem crescente
+print('-='*35)
+print('{:=^81}'.format('\33[30;42mValores únicos em lista\33[m'))
+print('-='*35)
+
+lista_valores_unicos = []
+
+while True:
+    numeros_para_adicionar = int(input('Digite um número: '))
+    if numeros_para_adicionar not in lista_valores_unicos:
+        lista_valores_unicos.append(numeros_para_adicionar)
+        sleep(0.33)
+        print('Valor adicionado com sucesso!')
+        if input('Deseja continuar? [S/N] ').upper() == 'N':
+            break
+    else:
+        sleep(0.33)
+        print('Valor duplicado! Não vou adicionar...')
+        if input('Deseja continuar? [S/N] ').upper() == 'N':
+            break
+print(f'Os valores digitados foram: ', end='')
+print(sorted(lista_valores_unicos))
+print('\033[1;30;41m}#{|\033[m' * 20)
+
+#desafio 80
+# Crie um programa onde o usuário possa digitar cinco valores numéricos e 
+# cadastre-os em uma lista
+# Já na posição correta de inserção (sem usar o sort())
+# No final, mostre a lista ordenada na tela
+print('-='*35)
+print('{:=^81}'.format('\33[30;42mLista ordenada sem sort()\33[m'))
+print('-='*35)
+
+lista_pre_ordenada = []
+
+for aux in range(0, 5):
+    numero_adicionar = int(input('Digite um número: '))
+    if len(lista_pre_ordenada) == 0:
+        lista_pre_ordenada.append(numero_adicionar)
+    
+
+#desafio 81
+# Crie um programa que vai ler vários números e colocar em uma lista
+# Depois disso, mostre:
+# A) Quantos números foram digitados
+# B) A lista de valores, ordenada de forma decrescente
+# C) Se o valor 5 foi digitado e está ou não na lista
+
+#desafio 82
+# Crie um programa que vai ler vários números e colocar em uma lista
+# Depois disso, crie duas listas extras que vão conter apenas os valores pares 
+# e os valores ímpares digitados, respectivamente
+# Ao final, mostre o conteúdo das três listas geradas
+
+#desafio 83
+# Crie um programa onde o usuário digite uma expressão qualquer que use parênteses
+# Seu aplicativo deverá analisar se a expressão passada está com os parênteses 
+# abertos e fechados na ordem correta
