436950e102cce1536471efa1f90f836dc27546f7
RaphaelKentha <137506515+RaphaelKentha@users.noreply.github.com>
2026-01-10 21:53:01 -0300

    ComecandoNoJava
    Primeiro teste em java.

---
+++ Java/PrimeiroCafe/src/main/java/com/mycompany/primeirocafe/PrimeiroCafe.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/*pacote*/
+package com.mycompany.primeirocafe;
+
+/**
+ *
+ * @author Kentha
+ */
+
+/* classe */
+public class PrimeiroCafe {
+
+    public static void main(String[] args) {
+        System.out.println("Escrevendo no java");
+    }
+}
